c6448a5869f35c3fc3134f283a53a29046850cfe
HansPhilippi <55080736+HansPhilippi@users.noreply.github.com>
2019-10-28 17:03:29 +0100

    Final project

---
+++ labs/final project/Shapes - ToTheBridge/WindowsFormsApp1/Rectangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Drawing;
+
+
+class Rectangle : Shape
+{
+    protected int Width { get; }
+    protected int Height { get; }
+
+    public Rectangle(int w, int h, Color c)
+    {
+      this.Width = w; this.Height = h;
+      this.edgeColor = c;
+      this.description = "rectangle";
+    }
+
+
+    public override void Draw(ShapeDisplay sd, Graphics canvas)
+    {
+       base.Draw(sd, canvas);
+
+        // determine position for drawing
+        int xStart = sd.Xoffset;
+        int yStart = sd.Yoffset + this.Height;
+
+        canvas.DrawLine(pen, xStart, yStart, xStart, yStart - Height);
+        canvas.DrawLine(pen, xStart, yStart, xStart + Width, yStart);
+        canvas.DrawLine(pen, xStart, yStart - Height, xStart + Width, yStart - Height);
+        canvas.DrawLine(pen, xStart + Width, yStart, xStart + Width, yStart - Height);
+    }
+}
+
